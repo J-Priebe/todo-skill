55cabd892756f0753253855fd4a61a681c50830b
James <jamespriebe92@gmail.com>
2021-01-25 23:09:39 -0500

    add some dialogs and handlers

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ import sqlite3
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
 
 # 'incomplete', 'complete', 'archived' (soft delete),
+# TODO define constants
 create_sql = '''
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY,
@@ -50,25 +51,25 @@ def add_item(item_description):
     conn.cursor().execute(sql)
     conn.commit()
 
-# TODO change status instead of hard deleting
-def delete_item_by_row_number(item_row_num):
+def close_item_by_row_number(item_row_num, close_status):
     # We cannot use PK for row number so we get the offset
     # and trust it's consistent since we are only reading/writing
     # from one local instance.
     # row number requires version 3. something and we only have 2.6.0
     # we can do this without row number simply by using offset
+    # TEST: duplicate descriptions where one is already closed
     offset = int(item_row_num) - 1
-    # TODO: parametrize
-    sql = f'''
-    DELETE FROM items
-    WHERE id IN
-    (
-    SELECT id FROM items LIMIT 1 OFFSET {offset}
-    )
+    sql = '''
+    UPDATE items
+    SET 
+        status = ?
+    WHERE status = 'incomplete'
+    LIMIT 1
+    OFFSET ?
     '''
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, (close_status, offset,))
     if cursor.rowcount == 1:
         conn.commit()
         conn.close()
@@ -77,20 +78,27 @@ def delete_item_by_row_number(item_row_num):
     return False
 
 
-def delete_item_by_description(item_description):
-    # TODO: tokenize to get better matches?
-    sql = f'''
-    DELETE FROM items
-    WHERE description LIKE '%{item_description}%'
+def close_item_by_description(item_description, close_status):
+    # TODO: tokenize description and construct
+    # ...OR LIKE... query to allow for partial matches
+    description_param = f'%{item_description}%'
+    sql = '''
+    UPDATE items
+    SET
+        status = ?
+    WHERE 
+        status = 'incomplete'
+        AND description LIKE ?
     '''
     conn = get_connection()
     cursor = conn.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, (close_status, description_param,))
     if cursor.rowcount == 1:
         conn.commit()
         conn.close()
         return True
     conn.close()
+
     return False
 
 
@@ -99,11 +107,29 @@ def get_active_items():
 
     sql = '''
     SELECT
-        description, status
+        description
     FROM items
-   
+    WHERE
+        status = 'incomplete'
     '''
 
     cursor = get_connection().cursor()
     cursor.execute(sql)
     return cursor.fetchall()
+
+
+def get_random_active_item():
+    sql = '''
+    SELECT
+        description
+    FROM items
+    WHERE
+        status = 'incomplete'
+    ORDER BY RANDOM()
+    LIMIT 1
+    '''
+    
+    cursor = get_connection().cursor()
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return result[0] if result else None
--- skill.py
+++ skill.py
@@ -22,54 +22,66 @@ class Todo(MycroftSkill):
 
 
     Because ToDos are so generic, and can basically span
-    the entirety of language, I think Padacious is a better choice
+    the entirety of language, I suspect Padacious is a better choice
     for intent parsing. Otherwise it will collide with other skills 
-    too frequently. We'll see
+    too frequently. We'll try both and see.
     
     '''
     def __init__(self):
         MycroftSkill.__init__(self)     
         db.init_db()
 
-    @intent_handler('list_items.intent')
+    # @intent_handler(IntentBuilder('suggest')) # Adapt-style
+    @intent_handler('list_items.intent') # Padacious-style
     def handle_list_items(self, message):
         items = db.get_active_items()
-        self.log.info('ITEMS:')
-        self.log.info(items)
-        # todo factor into dialog file
-        self.speak_dialog('Here is your current to-do list')
+        if not items:
+            self.speak_dialog('empty_list')
+            return
+    
+        self.speak_dialog('list_items')
         # row_number/partitioning requires version 3.x of sqlite3, 
         # so we simply enumerate in code
         for (i, item) in enumerate(items):
             self.speak_dialog(
-                # e.g., Item 1: Take out the trash
-                f'Item {i + 1}: {item[0]}'
+                'item_description', 
+                {
+                    'item_number':i+1, 
+                    'item_description': item[0]
+                }
             )
 
     @intent_handler('add_item.intent')
     def handle_add_item(self, message):
         item_description = message.data.get('item')
         db.add_item(item_description)
-        self.speak_dialog(f'I have added {item_description} to your list.')
+        self.speak_dialog('item_added', {'item_description': item_description})
 
-    # @intent_handler(IntentBuilder('suggest')) # Adapt
     @intent_handler('suggest_item.intent') # Padacious
     def handle_suggest_item(self, message):
-        self.speak_dialog('suggest')
-        items = db.get_active_items()
-        # now we suggest the longest running item
+        suggested_item = db.get_random_active_item()
+        if suggested_item is None:
+            self.speak_dialog('empty_list')
+        else:
+            self.speak_dialog(
+                'suggest_item', 
+                {
+                    'item_description': suggested_item
+                }
+            )
 
+    # delete kind of sounds like complete... just how good is the STT :)              
     @intent_handler('delete_item.intent')
     def handle_delete_item(self, message):
         item_number = message.data.get('item_number')
         item_description = message.data.get('item_description')
 
-        # TODO: confirmation converse
+        # TODO: confirmation followup conversation
         success = False
         if item_number:
-            success = db.delete_item_by_row_number(item_number)
+            success = db.close_item_by_row_number(item_number, 'archived')
         elif item_description:
-            success = db.delete_item_by_description(item_description)
+            success = db.close_item_by_description(item_description, 'archived')
         
         if success:
             self.speak_dialog('I deleted that for you')
@@ -79,12 +91,31 @@ class Todo(MycroftSkill):
 
     @intent_handler('complete_item.intent')
     def handle_complete_item(self, message):
-        db.complete_item()
+        item_number = message.data.get('item_number')
+        item_description = message.data.get('item_description')
+
+        success = False
+        if item_number:
+            success = db.close_item_by_row_number(item_number, 'complete')
+        elif item_description:
+            success = db.close_item_by_description(item_description, 'complete')
+        
+        if success:
+            self.speak_dialog('I marked that one off for you. Nice job!')
+        else:
+            self.speak_dialog('Sorry, I could not do that for you')
+
+    # TODO
 
     @intent_handler('report.intent')
     def handle_report(self, message):
         num_completed = db.get_num_completed_items()
         # average time to completion
+        # total completed
+        # completed per week
+        self.speak_dialog('coming soon')
 
 
-    # TODO help request
+    @intent_handler('help.intent')
+    def handle_help(self, message):
+        self.speak_dialog('coming soon')
