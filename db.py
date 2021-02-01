ecfbe80a98a1832d3898323c94f38fedb1133de9
James <jamespriebe92@gmail.com>
2021-01-31 22:41:19 -0500

    some polish; add help and report handlers

--- db.py
+++ db.py
@@ -1,22 +1,26 @@
 import os
 import sqlite3
+from datetime import datetime
+
 
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
 
+
 # 'incomplete', 'complete', 'archived' (soft delete),
-# TODO define constants
 create_sql = '''
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY,
     description TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'incomplete',
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    completed_at TIMESTAMP
+    closed_at TIMESTAMP
 )
 '''
 
+
 def get_connection(db_path=DEFAULT_PATH):
     return sqlite3.connect(db_path)
+
 
 def init_db():
     conn = get_connection()
@@ -28,8 +32,6 @@ def init_db():
 
 
 def add_item(item_description):
-    conn = get_connection()
-    
     sql = f'''
     INSERT INTO items (
         description
@@ -37,18 +39,19 @@ def add_item(item_description):
     VALUES (
         ?
     )
-
     '''
-
+    conn = get_connection()
     conn.cursor().execute(sql, (item_description,))
     conn.commit()
 
-def fetch_item(log, row_number):
-    log.info(row_number)
+
+def fetch_item_by_row_number(row_number):
     offset = int(row_number) - 1
 
     sql = '''
-    SELECT id, description
+    SELECT 
+        id, 
+        description
     FROM items
     WHERE status = 'incomplete'
     LIMIT 1
@@ -58,15 +61,64 @@ def fetch_item(log, row_number):
     cursor = conn.cursor()
     cursor.execute(sql, (offset,))
     return cursor.fetchone()
-    #return result[0] if result else None
 
 
-def close_item(log, item_id, close_status):
-    log.info(f'item id: {item_id}, close status: {close_status}')
+def fetch_item_by_description(description):
+    '''
+    Looks for an exact on the provided item description.
+    If nothing is found, tokenizes the description
+    and looks for an unambiguous partial match.
+    '''
+
+    full_match_sql = '''
+    SELECT 
+        id, 
+        description
+    FROM items
+    WHERE 
+        status = 'incomplete'
+        AND description LIKE ?
+    '''
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(full_match_sql, (description,))
+    full_match = cursor.fetchone()
+    if full_match is not None:
+        return full_match
+
+    tokens = [
+        f'%{t}%' for t in description.split(' ')
+    ]
+    like_query = ' OR '.join(
+        ['description LIKE ?' for t in tokens]
+    )
+    partial_match_sql = f'''
+    SELECT 
+        id, 
+        description
+    FROM items
+    WHERE 
+        status = 'incomplete'
+        AND (
+            {like_query}
+        )
+    '''
+    cursor.execute(partial_match_sql, tuple(tokens))
+    # We're doing a pretty generous match, so only
+    # return a result if it's unambiguous
+    results = cursor.fetchall()
+    if len(results) == 1:
+        return results[0]
+    return None
+
+
+def close_item(item_id, close_status):
     sql = '''
     UPDATE items
     SET 
-        status = ?
+        status = ?,
+        closed_at = CURRENT_TIMESTAMP
     WHERE id = ?
     LIMIT 1
     '''
@@ -76,56 +128,6 @@ def close_item(log, item_id, close_status):
     if cursor.rowcount == 1:
         conn.commit()
         return True
-    return False
-
-def close_item_by_row_number(item_row_num, close_status):
-    # We cannot use PK for row number so we get the offset
-    # and trust it's consistent since we are only reading/writing
-    # from one local instance.
-    # row number requires version 3. something and we only have 2.6.0
-    # we can do this without row number simply by using offset
-    # TEST: duplicate descriptions where one is already closed
-    offset = int(item_row_num) - 1
-    sql = '''
-    UPDATE items
-    SET 
-        status = ?
-    WHERE status = 'incomplete'
-    LIMIT 1
-    OFFSET ?
-    '''
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(sql, (close_status, offset,))
-    if cursor.rowcount == 1:
-        conn.commit()
-        conn.close()
-        return True
-    conn.close()
-    return False
-
-
-def close_item_by_description(item_description, close_status):
-    # TODO: tokenize description and construct
-    # ...OR LIKE... query to allow for partial matches
-    description_param = f'%{item_description}%'
-    sql = '''
-    UPDATE items
-    SET
-        status = ?
-    WHERE 
-        status = 'incomplete'
-        AND description LIKE ?
-    '''
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(sql, (close_status, description_param,))
-    if cursor.rowcount == 1:
-        conn.commit()
-        conn.close()
-        return True
-    conn.close()
-
     return False
 
 
@@ -172,17 +174,51 @@ def get_num_completed_items():
     
     cursor = get_connection().cursor()
     cursor.execute(sql)
-    return cursor.fetchone()
+    return cursor.fetchone()[0]
 
 
-def get_average_completed_per_week():
-    # a little tricky. need to know
-    # # completed, timeframe (earliest started and now?),
-    # and timeframe converted to # of weeks
-    pass
-
-
-def get_average_time_to_completion():
+def get_earliest_created_time():
+    sql = '''
+    SELECT created_at
+    FROM items
+    ORDER BY created_at
+    LIMIT 1
     '''
+    cursor = get_connection().cursor()
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    if res is None:
+        return None
+    return datetime.strptime(res[0], '%Y-%m-%d %H:%M:%S')
 
+
+def get_latest_completed_time():
+    sql = '''
+    SELECT closed_at
+    FROM items
+    WHERE status = 'complete'
+    ORDER BY closed_at DESC
+    LIMIT 1
     '''
+    cursor = get_connection().cursor()
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    if res is None:
+        return None
+    return datetime.strptime(res[0], '%Y-%m-%d %H:%M:%S')
+
+
+def get_average_days_to_completion():
+    sql = '''
+    SELECT 
+        AVG( julianday(closed_at) - julianday(created_at) )
+    FROM items
+    WHERE status = 'complete'
+    '''
+    cursor = get_connection().cursor()
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    if res is None:
+        return None
+        
+    return round(res[0])
--- skill.py
+++ skill.py
@@ -1,35 +1,26 @@
 from mycroft import MycroftSkill, intent_handler
 from adapt.intent import IntentBuilder
-
+import math
 from . import db
 
 class Todo(MycroftSkill):
     '''
-    What's on my todo list?
-    - lists all active tasks
-
-    What should I do today?
-    - suggests a todo item that has been on your list 
-      the longest
-
-    Delete/Remove/Archive item # from my to do list
-
-    Mark item # as complete/Complete item # on my todo list
-
-    Give me stats on my todo list
-    - # of tasks completed
-    - average completion time
-
-
-    Because ToDos are so generic, and can basically span
-    the entirety of language, I suspect Padacious is a better choice
-    for intent parsing. Otherwise it will collide with other skills 
-    too frequently. We'll try both and see.
+    Simple to-do list manager that supports the following actions:
+    - List items on you to-do list
+    - Complete items
+    - Delete (archive) items
+    - Suggest a random item
+    - Report completion statistics
     
+    Uses Padicious-style intents. Because a to-do item is so generic,
+    I suspect Padacious is a better choice for intent parsing. 
+    Otherwise it will collide with other skills too frequently.
     '''
+
     def __init__(self):
         MycroftSkill.__init__(self)     
         db.init_db()
+
 
     # @intent_handler(IntentBuilder('suggest')) # Adapt-style
     @intent_handler('list_items.intent') # Padacious-style
@@ -40,8 +31,8 @@ class Todo(MycroftSkill):
             return
     
         self.speak_dialog('list_items')
-        # row_number/partitioning requires version 3.x of sqlite3, 
-        # so we simply enumerate in code
+        # row_number/partitioning requires version 3.x of sqlite3; 
+        # just enumerate in code
         for (i, item) in enumerate(items):
             self.speak_dialog(
                 'item_description', 
@@ -51,13 +42,15 @@ class Todo(MycroftSkill):
                 }
             )
 
+
     @intent_handler('add_item.intent')
     def handle_add_item(self, message):
         item_description = message.data.get('item')
         db.add_item(item_description)
         self.speak_dialog('item_added', {'item_description': item_description})
 
-    @intent_handler('suggest_item.intent') # Padacious
+
+    @intent_handler('suggest_item.intent') 
     def handle_suggest_item(self, message):
         suggested_item = db.get_random_active_item()
         if suggested_item is None:
@@ -70,67 +63,145 @@ class Todo(MycroftSkill):
                 }
             )
 
-    # delete kind of sounds like complete... just how good is the STT :)              
+
     @intent_handler('delete_item.intent')
     def handle_delete_item(self, message):
-        # TODO: item number one is read in a literally "one"
-        item_number = message.data.get('item_number')
-        item_description = message.data.get('item_description')
-
-        item = db.fetch_item(self.log, item_number)
-        self.log.info(item)
-
+        identifier = message.data.get('item')
+        item = self.fetch_item(identifier)
         if item is None:
-            self.speak_dialog('could not find that')
             return
         
         pk, description = item
-        if not self.confirm_closure(description):
-            self.speak_dialog("OK, I won't delete that.")
+        if not self.confirm_delete(description):
+            self.speak_dialog("Okay, I won't delete that.")
             return
         
-        success = db.close_item(self.log, pk, 'archived')
-
-
+        success = db.close_item(pk, 'archived')
         if success:
-            self.speak_dialog('I deleted that for you')
+            self.speak_dialog(f'Okay, I deleted {identifier} for you')
         else:
-            self.speak_dialog('Sorry, I could not do that for you')
+            self.speak_dialog(f'Sorry, I encountered a problem deleting {identifier}')
 
 
     @intent_handler('complete_item.intent')
     def handle_complete_item(self, message):
-        item_number = message.data.get('item_number')
-        item_description = message.data.get('item_description')
-
-        success = False
-        if item_number:
-            success = db.close_item_by_row_number(item_number, 'complete')
-        elif item_description:
-            success = db.close_item_by_description(item_description, 'complete')
+        identifier = message.data.get('item')
+        item = self.fetch_item(identifier)
+        if item is None:
+            return
         
+        pk, description = item
+        success = db.close_item(pk, 'complete')
         if success:
             self.speak_dialog('I marked that one off for you. Nice job!')
         else:
-            self.speak_dialog('Sorry, I could not do that for you')
+            self.speak_dialog(f'Sorry, I encountered a problem completing {identifier}')
 
-    # TODO
 
     @intent_handler('report.intent')
     def handle_report(self, message):
+        '''
+        Provide a report on a few to-do statistics:
+        - Number of items completed
+        - Average number of items completed per week
+        - Average time (in days) to complete an item
+        '''
         num_completed = db.get_num_completed_items()
-        # average time to completion
-        # total completed
-        # completed per week
-        self.speak_dialog('coming soon')
+
+        self.speak_dialog(f'You have completed {num_completed} items.')
+
+        earliest_created = db.get_earliest_created_time()
+        latest_completed = db.get_latest_completed_time()
+        if earliest_created and latest_completed:
+            weeks_delta  = math.ceil(
+                (latest_completed - earliest_created).total_seconds() / 604800 # seconds per week
+            )
+            self.log.info(f'WEEKS DELTA: {weeks_delta}')
+            average_completed_per_week = round(
+                num_completed / weeks_delta
+            )
+            average_days_to_completion = db.get_average_days_to_completion()
+            self.speak_dialog(
+                f'On average you complete {average_completed_per_week} items per week'
+                f' and take approximately {average_days_to_completion} days to complete'
+                ' each item.'
+            )
 
 
     @intent_handler('help.intent')
     def handle_help(self, message):
-        self.speak_dialog('coming soon')
+        self.speak_dialog(
+            'With this skill I can help you manage your to do list.'
+        )
+        self.speak_dialog(
+            'I can add, complete, or cancel items, suggest a random task'
+            ' for you to work on, or give you a report on your completed tasks.'
+        )
+        self.speak_dialog(
+            'Here are some examples.'
+        )
+        self.speak_dialog(
+            'Give me my to do list'
+        )
+        self.speak_dialog(
+            'Put go to the gym on my to do list'
+        )
+        self.speak_dialog(
+            'Cancel item number 1 on my to do list'
+        )
+        self.speak_dialog(
+            'Finish item prepare for job interview on my to do list'
+        )
+        self.speak_dialog(
+            'What should I do today'
+        )
+        self.speak_dialog(
+            'Report on to do'
+        )
+
+    def parse_item_number(self, identifier):
+        '''
+        Try to parse a number from an item descriptor.
+        Sometimes padacious will parse 1 as "one", 2 as "to", etc,
+        so we add some explicit handling for those cases.
+        '''
+        tokens = identifier.split(' ')
+        # multi-word identifier can't be a number
+        if len(tokens) != 1:
+            return None
+
+        identifier = tokens[0]
+        
+        if identifier in ('one', 'won'):
+            return 1
+        
+        if identifier in ('to', 'too', 'two'):
+            return 2
+        
+        if identifier in ('for', 'four', 'fore'):
+            return 4
+        
+        try: 
+            return int(identifier)
+        except Exception:
+            return None
 
 
-    def confirm_closure(self, item):
-        resp = self.ask_yesno(f'are you sure I should delete item {item}')
-        # can this be customized?
+    def fetch_item(self, identifier):
+        # attempt to parse out a number (the item's position in the list)
+        # from the identifier
+        item_number = self.parse_item_number(identifier)
+        if item_number:
+            return db.fetch_item_by_row_number(item_number)
+        
+        # and fall back to matching based on a description
+        item = db.fetch_item_by_description(identifier)
+        if item is None:
+            self.speak_dialog(f"I couldn't find {identifier} on your to do list")
+
+        return item
+
+
+    def confirm_delete(self, item):
+        resp = self.ask_yesno(f'Are you sure you want to delete item {item}')
         return resp == 'yes'
